984cbe25935948950e3f7c094a99f4975d53316a
pixlflip <pixlflipofficial@gmail.com>
2022-01-15 23:06:01 -0600

    bug fix

--- SensoryNode.py
+++ SensoryNode.py
@@ -1,7 +1,7 @@
 # Once active run forever a set of all sensors connected to the device and store the data for easy retrieval
 """Super primitive for now but will systematically post latest information to database for other functions to use."""
 import Adafruit_DHT
-sensor = Adafruit_DHT.DHT11
+sensor = Adafruit_DHT.DHT22
 pin = 4
 while True:
      humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
