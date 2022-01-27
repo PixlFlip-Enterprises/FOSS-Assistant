adee8fa93126c2bdf5cacb3dba48094e0ab8e3ae
pixlflip <pixlflipofficial@gmail.com>
2022-01-27 10:47:22 -0600

    possible fix

--- SensoryNode.py
+++ SensoryNode.py
@@ -22,8 +22,35 @@ I taking the time to write this???
 import pandas as pd
 import numpy
 from pytrends.request import TrendReq
-pytrend = TrendReq()
-# Get Google Hot Trends data
-df = pytrend.trending_searches(pn='united_states')
-# df = pytrend.today_searches(pn='US')
-df.head()
+# NOTE: must use pip3 import newspaper3k for it to import correctly
+import newspaper
+from newspaper import Article
+
+# # Daily Briefing Builder
+# # setup object
+# pytrend = TrendReq()
+# # Get Google Hot Trends data
+# df = pytrend.trending_searches(pn='united_states')
+# # turn that into array
+# temp_trending = df.values
+# # format array into string so we can actually use it
+# google_trending_searches = []
+# for trend in temp_trending:
+#     google_trending_searches.append(trend.__str__().replace("'", "").replace("[", "").replace("]", ""))
+#     print(trend.__str__().replace("'", "").replace("[", "").replace("]", ""))
+# # Google Trend Data Retrieved
+
+site = newspaper.build("https://news.ycombinator.com/news", memoize_articles=False)
+# site = newspaper.build("https://www.wnd.com/", memoize_articles=False)
+# get list of article URLs
+article_urls = site.article_urls()
+top_articles = []
+# sort data into array
+for index in range(10):
+    article = site.articles[index]
+    article.download()
+    article.parse()
+    top_articles.append(article)
+    print(article_urls[index])
+# Get Trending Searches
+print(newspaper.hot())
