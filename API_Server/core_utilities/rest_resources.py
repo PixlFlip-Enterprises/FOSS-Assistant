d53c78786b8deebb210c140824d7cd4395549b85
pixlflip <pixlflipofficial@gmail.com>
2022-07-08 14:40:04 -0500

    rest_resources.py work

--- API_Server/core_utilities/rest_resources.py
+++ API_Server/core_utilities/rest_resources.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource, reqparse
-
+from datetime import datetime
+import pymysql as MySQLdb
 # todo this should be gone and be fully functional as a package.
 from API_Server.Functions import Protocols, User, Email
 
@@ -36,16 +37,30 @@ class Journal(Resource):
         # verify session
         if User.is_profile_api_key(args['session_token']) == False:
             return {"status": "failed. Invalid session token"}
-        # load profile from session token
-        PROFILE = User.Profile(User.is_profile_api_key(args['session_token']))
         # log
         Protocols.debug_log(console_printout="Journal View Entry", user=User.is_profile_api_key(args['session_token']), command="000020", method_of_input="REST API")
+        try:
+            # open the database
+            db = MySQLdb.connect(host="localhost", user=SQLUSERNAME, password=SQLPASSWORD, database=SQLDATABASE)
+            cursor = db.cursor()
+            # Execute the SQL command
+            cursor.execute("select * from JOURNAL where PROFILE_ID LIKE '" + User.is_profile_api_key(args['session_token']) + "' AND DATE LIKE '" + args['date'] + "%';")
+            # get all records
+            records = cursor.fetchall()
+            # first is entry
+            try:
+                entry = records[0]
+                return {"date": entry[2], "entry": entry[3], "starred": entry[5],"creation_device": entry[6], "timezone": entry[7]}, 201
+            except:
+                return {"status": "Failed. Entry does not exist"}
+        except:
+            # Rollback in case there is any error
+            db.rollback()
+        # disconnect from server
+        db.close()
+        # return since entry doesn't exist
+        return {"status": "Failed. Entry does not exist"}
 
-        # Get the entry
-        entry = PROFILE.journal.get_entry(args['date'])
-        # build json to return
-        return {"date": entry.date, "entry": entry.entry, "starred": entry.starred,
-                "creation_device": entry.creationDevice, "timezone": entry.timeZone}, 201
 
     def put(self):
         # verify fields
