eab6d2f3b3c48c022c6eab7941f7b623ba9c1bb4
pixlflip <55899967+pixlflip@users.noreply.github.com>
2022-08-02 15:50:53 -0500

    more gui work

--- Desktop Client/__main__.py
+++ Desktop Client/__main__.py
@@ -6,6 +6,7 @@ from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
+from kivy.uix.checkbox import CheckBox
 import json
 import sys
 import requests
@@ -18,16 +19,44 @@ API_KEY = '#2AJKLFHW9203NJFC'
 class MainMenu(App):
     def build(self):
         Window.size = (500, 800)
-        main_layout = BoxLayout(orientation="vertical")
-        button = Button(text="Journal",pos_hint={"center_x": 0.5, "center_y": 0.5},)
-        button.bind(on_press=self.on_journal_button_press)
-        main_layout.add_widget(button)
+        # returns a window object with all it's widgets
+        self.window = GridLayout()
+        self.window.cols = 1
+        self.window.size_hint = (0.8, 1)
+        self.window.pos_hint = {"center_x": 0.5, "center_y": 0.5}
 
-        equals_button = Button(text="Profile", pos_hint={"center_x": 0.5, "center_y": 0.5})
-        equals_button.bind(on_press=self.on_profile_button_press)
-        main_layout.add_widget(equals_button)
+        # image widget
+        self.window.add_widget(Image(source="logo.png"))
 
-        return main_layout
+        # label as a spacer
+        self.s_label2 = Label(text=" ", font_size=10, color='#43ccc5')
+        self.window.add_widget(self.s_label2)
+
+        # button widget
+        self.button = Button(text="Journal", size_hint=(0.2, 0.2), bold=True,
+                             background_color='#43CCC5',
+                             background_normal="#43CCC5"
+                             )
+        self.button.bind(on_press=self.on_journal_button_press)
+        self.window.add_widget(self.button)
+
+        # label as a spacer
+        self.s_label3 = Label(text=" ", font_size=10, color='#43ccc5')
+        self.window.add_widget(self.s_label3)
+
+        # button widget
+        self.profile_button = Button(text="Profile", size_hint=(0.2, 0.2), bold=True,
+                             background_color='#43CCC5',
+                             background_normal="#43CCC5"
+                             )
+        self.profile_button.bind(on_press=self.on_profile_button_press)
+        self.window.add_widget(self.profile_button)
+
+        # label as a spacer
+        self.s_label4 = Label(text=" ", font_size=10, color='#43ccc5')
+        self.window.add_widget(self.s_label4)
+
+        return self.window
 
     def on_journal_button_press(self, instance):
         # kill current app and launch journal
@@ -44,23 +73,58 @@ class MainMenu(App):
 
 class JournalPage(App):
     def build(self):
-        main_layout = BoxLayout(orientation="vertical")
+        Window.size = (500, 800)
+        # returns a window object with all it's widgets
+        self.window = GridLayout()
+        self.window.cols = 1
+        self.window.size_hint = (0.8, 1)
+        self.window.pos_hint = {"center_x": 0.5, "center_y": 0.5}
+
+        # label widget
+        self.user_label = Label(text="Journal", font_size=40, color='#43ccc5', bold=True)
+        self.window.add_widget(self.user_label)
+
         # entry text input
         self.entry = TextInput(multiline=False, readonly=False, halign="center", font_size=50)
-        main_layout.add_widget(self.entry)
+        self.window.add_widget(self.entry)
+
+        # TODO To make a more detailed area using Kivy you just can create another grid and add it back as a widget. Mind blown
+        self.subgrid = GridLayout()
+        self.subgrid.cols = 2
+        # Add checkbox, Label and Widget
+        self.subgrid.add_widget(Label(text='Favorite:', font_size=20, color='#43ccc5'))
+        self.starred = CheckBox(active=False)
+        self.subgrid.add_widget(self.starred)
+        self.window.add_widget(self.subgrid)
+
+
         # entry record button
-        entry_button = Button(text="Record Entry", pos_hint={"center_x": 0.5, "center_y": 0.5}, )
-        entry_button.bind(on_press=self.on_entry_button_press)
-        main_layout.add_widget(entry_button)
+        self.entry_button = Button(text="Record Entry", size_hint=(0.2, 0.2), bold=True,
+                             background_color='#43CCC5',
+                             background_normal="#43CCC5"
+                             )
+        self.entry_button.bind(on_press=self.on_entry_button_press)
+        self.window.add_widget(self.entry_button)
+
+        # label as a spacer
+        self.s_label = Label(text=" ", font_size=10, color='#43ccc5')
+        self.window.add_widget(self.s_label)
+
         # back button
-        back_button = Button(text="Back", pos_hint={"center_x": 0.5, "center_y": 0.5}, )
-        back_button.bind(on_press=self.on_back_button_press)
-        main_layout.add_widget(back_button)
-        return main_layout
+        self.back_button = Button(text="Back", size_hint=(0.2, 0.2), bold=True,
+                                   background_color='#43CCC5',
+                                   background_normal="#43CCC5"
+                                   )
+        self.back_button.bind(on_press=self.on_back_button_press)
+        self.window.add_widget(self.back_button)
+
+
+        return self.window
 
     def on_entry_button_press(self, instance):
         entry = self.entry.text
         # todo api call to save entry
+        # todo use try catch for error handling (because believe you me it will throw errors until I fix api calls)
         response = requests.put(BASE + "journal",
                                 json={'session_token': API_KEY, 'date': '2022-07-16', 'entry': entry,
                                       'creation_device': 'Kivy Desktop Client', 'starred': 'false', 'time_zone': 'EST'})
