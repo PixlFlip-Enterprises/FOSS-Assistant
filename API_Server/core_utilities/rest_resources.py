ffd6effae1206ba6576b9c24897effeecbfa2fd8
pixlflip <pixlflipofficial@gmail.com>
2022-09-09 13:52:42 -0500

    mafs

--- API_Server/core_utilities/rest_resources.py
+++ API_Server/core_utilities/rest_resources.py
@@ -343,3 +343,136 @@ class Login(Resource):
         # update session token list in database and set to expire in 24 hours
 
         return {"status": "Completed", "session_token": session_token }
+
+
+########################################################################################################################
+#                                                   FINANCE RESOURCE
+########################################################################################################################
+# finance get args
+finance_get_args = reqparse.RequestParser()
+finance_get_args.add_argument("session_token", type=str, help="Token from valid session. Expired token?", required=True)
+finance_get_args.add_argument("date", type=str, help="Date of finance entry", required=True)
+finance_get_args.add_argument("device_id", type=str, help="Device created on", required=False)
+
+# finance put args
+finance_put_args = reqparse.RequestParser()
+finance_put_args.add_argument("session_token", type=str, help="Token from valid session. Expired token?", required=True)
+finance_put_args.add_argument("date", type=str, help="Date of finance entry", required=False)
+finance_put_args.add_argument("title", type=str, help="finance entry", required=True)
+finance_put_args.add_argument("memo", type=str, help="Device created on", required=False)
+finance_put_args.add_argument("starred", type=str, help="Favorited or not", required=True)
+# finance del args
+finance_del_args = reqparse.RequestParser()
+finance_del_args.add_argument("session_token", type=str, help="Token from valid session. Expired token?", required=True)
+finance_del_args.add_argument("date", type=str, help="Date of finance entry to be deleted", required=True)
+
+
+# create finance resource
+class Finance(Resource):
+
+    def get(self):
+        # verify fields
+        args = finance_get_args.parse_args()
+        # verify session
+        if core_functions.is_profile_api_key(args['session_token']) == False:
+            return {"status": "failed. Invalid session token"}
+        try:
+            # open the database
+            db = MySQLdb.connect(host="localhost", user=config['sql_username'], password=config['sql_password'], database=config['sql_database'])
+            cursor = db.cursor()
+            # Execute the SQL command
+            cursor.execute("select * from FINANCE where PROFILE_ID LIKE '" + core_functions.is_profile_api_key(args['session_token']) + "' AND DATE LIKE '" + args['date'] + "%';")
+            # get all records
+            records = cursor.fetchall()
+            # first is entry
+            try:
+                entry = records[0]
+                # log
+                core_functions.debug_log(user=core_functions.is_profile_api_key(args['session_token']), command_run="Finance View Entry", device_id=args['device_id'], command_id="000040", return_code="201")
+                return {"status": "Completed.", "date": entry[2], "entry": entry[3], "starred": entry[5], "creation_device": entry[6], "timezone": entry[7]}, 201
+            except:
+                # log
+                core_functions.debug_log(user=core_functions.is_profile_api_key(args['session_token']), command_run="Finance View Entry", device_id=args['device_id'], command_id="000040", return_code="404", debug_msg="Error while retrieving the entry")
+                return {"status": "Failed. Entry does not exist"}
+        except:
+            # Rollback in case there is any error
+            db.rollback()
+        # disconnect from server
+        db.close()
+        # return since entry doesn't exist
+        core_functions.debug_log(user=core_functions.is_profile_api_key(args['session_token']), command_run="Finance View Entry", device_id=args['device_id'], command_id="000040", return_code="404", debug_msg="Entry was not found in the database")
+        return {"status": "Failed. Entry does not exist"}
+
+
+    def put(self):
+        # verify fields
+        args = finance_put_args.parse_args()
+        # verify session
+        if core_functions.is_profile_api_key(args['session_token']) == False:
+            return {"status": "Failed.", 'error_msg': " Invalid session token"}
+
+        username = core_functions.is_profile_api_key(args['session_token'])
+        # log
+        # todo add a thing in here to allow for arbitrary dates to be added instead of just the second the put is received
+        x = datetime.now().__str__().replace(" ", "")
+        try:
+            # open the database
+            db = MySQLdb.connect(host="localhost", user=config['sql_username'], password=config['sql_password'], database=config['sql_database'])
+            cursor = db.cursor()
+            # Execute the SQL command
+            sql = "INSERT INTO FINANCE (OWNING_USER, DATE, TITLE, MEMO, DEPOSIT, AMOUNT, ACCOUNT_BALANCE) VALUES(%s, %s, %s, %s, %s, %s, %s)"
+            # TODO maths here
+            val = (username, x, args['title'], args['memo'], args['deposit'], args['amount'], (int(args['amount']))-9)
+            print(val)
+            cursor.execute(sql, val)
+            # Commit your changes in the database
+            db.commit()
+        except:
+            # Rollback in case there is any error
+            db.rollback()
+            return {"status": "Failed. Is the data provided correct?"}
+
+        # disconnect from server
+        db.close()
+        # note, you can put a comma and then follow it with a status code of your choice. Handy for some stuff
+        return {"status": "Completed"}, 201
+
+
+    def delete(self):
+        # verify fields
+        args = finance_del_args.parse_args()
+        # verify session
+        if core_functions.is_profile_api_key(args['session_token']) == False:
+            return {"status": "failed. Invalid session token"}
+        # log
+        username = core_functions.is_profile_api_key(args['session_token'])
+
+        try:
+            # open the database
+            db = MySQLdb.connect(host="localhost", user=config['sql_username'], password=config['sql_password'], database=config['sql_database'])
+            cursor = db.cursor()
+            # Execute the SQL command
+            sql = "DELETE FROM finance WHERE PROFILE_ID = '" + username + "' AND DATE = '" + args['date'] + "%'"
+            cursor.execute(sql)
+            # Commit your changes in the database
+            db.commit()
+        except:
+            # Rollback in case there is any error
+            db.rollback()
+            core_functions.debug_log(user=core_functions.is_profile_api_key(args['session_token']),
+                                     command_run="finance Delete Entry", device_id=args['device_id'], command_id="000023",
+                                     return_code="404", debug_msg="Failed to delete entry.")
+            return {"status": "Failed. Most likely means the entry does not exist or bad input."}
+        return {"status": "Completed"}
+
+
+    def patch(self):
+        # verify fields
+        args = finance_get_args.parse_args()
+        # verify session
+        if core_functions.is_profile_api_key(args['session_token']) == False:
+            return {"status": "failed. Invalid session token"}
+
+        # log
+        core_functions.debug_log(user=core_functions.is_profile_api_key(args['session_token']), command_run="finance Delete Entry", device_id=args['device_id'], command_id="000023", return_code="201", debug_msg="Entry was deleted.")
+        return {"status": "Code will go here, but this returned correctly?"}
